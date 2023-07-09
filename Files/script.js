3d2d8d97557e27dd91d7577d2142a11edab31caa
Kenneth Darryl Cata <catakennethdarryl@gmail.com>
2023-07-09 12:43:39 +0800

    portfolio

---
+++ Files/script.js
@@ -0,0 +1,10 @@
+var Content;
+
+function Loading() {
+  Content = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
